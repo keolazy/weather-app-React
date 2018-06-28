33a900ae3b80a883e949865099715d8661cd3e55
Nathan Keolasy <keolazy1@gmail.com>
2018-06-28 15:00:25 -0500

    defined state in app without traditional constructor(props) setup & setState to proper res.data targets

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Weather from "./components/Weather"
 const API_KEY = "e952f7ce430503e397d8b4c21fda5ceb";
 
 class App extends React.Component {
+  // State is an object that lives within a component.
+  // Responsible for keepign track of chagnign data within a component 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   // Create own Method. arrow functions have this bound to App.
   getWeather = async (e) => {
     // prevents default behavior of button submitting;
@@ -18,7 +29,17 @@ class App extends React.Component {
     const data = await api_call.json();
     console.log(data);
     // Props are like html attributes. name them whatever you want. set them as JS expressions.
+    // Set values of state. Don't ever directly manipulate "state"
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
+
   render() {
     return (
       <div>
