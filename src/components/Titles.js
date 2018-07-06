85fefce93aa7c7df2a36198bcd23345bdff7d032
Nathan Keolasy <keolazy1@gmail.com>
2018-07-06 13:11:48 -0500

    styling added for weather key and values

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -21,7 +21,7 @@ import React from 'react';
 const Titles = () => (
   <div>
     <h1 className="title-container__title">Weather Finder</h1>
-    <p>Find out temperature, conditions, and more...</p>
+    <h3 className="title-container__subtitle">Find out temperature, conditions, and more...</h3>
 </div>
 );
 
